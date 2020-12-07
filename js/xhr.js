b7edf5bc2ff5f316acdca46a54bf317b6913dacc
dima Vlasenko <dimavlasenko3541@gmail.com>
2020-12-07 22:08:54 +0200

    request

---
+++ js/xhr.js
@@ -0,0 +1,20 @@
+const getData = (url) =>
+  fetch(url)
+    .then((Response) => Response.json())
+    .then((json) => {
+      if (json.Search) return json.Search;
+      throw Error("Сервер вернул неправильный обьект");
+    });
+
+
+inputSearch.addEventListener("keyup", (e) => {
+  delay(() => {
+    const searchString = e.target.value;
+    if (searchString && searchString.length > 4)
+      if (!triggerMode) clearMoviesMarcup();
+
+    getData(`http://www.omdbapi.com/?s=${searchString}&apikey=c71c5bd0`)
+      .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
+      .catch((err) => console.log(err));
+  }, 1000);
+});
